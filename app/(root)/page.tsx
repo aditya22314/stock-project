578dae5215706862bf43b9eb8a1b4fe45fc6eb1a
aditya22314 <tejusadianu500@gmail.com>
2026-02-06 18:49:43 +0530

    fix review comments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-      <Button>Click me </Button>
+      <Button>Click me</Button>
     </div>
   )
 }
